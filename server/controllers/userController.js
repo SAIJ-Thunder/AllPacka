669221f5314f76ff0fe969e728ade45a3e53f4b7
William Murphy <williamjmurphy@gmial.com>
2023-04-07 17:43:47 -0500

    added gitignore

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,29 +1,83 @@
 
-const { User, Trip } = require('./tripModel.js');
+const { User, Trip, Session } = require('./models.js');
 
 
 const userController = {};
 
-characterController.createUser = (req, res, next) => {
-    console.log('---We are in createCharacter in userController.js--');
-    const { username, password, trips  } = req.body; 
-    
-    const newUser = { 
-                username,
-                password,
-                trips,
-                }
+// helper function to create fileController error objects
+// return value will be the object we pass into next, invoking global error handler
+const createErr = (errInfo) => {
+    const { method, type, err } = errInfo;
+    return { 
+      log: `userController.${method} ${type}: ERROR: ${typeof err === 'object' ? JSON.stringify(err) : err}`,
+      message: { err: `Error occurred in userController.${method}. Check server logs for more details.` }
+    };
+};
 
-    res.locals.newUser = newUser;
-    return next();
+
+userController.getUser = (req, res, next) => {
+    console.log('---We are in getUser in userController.js--');
+
+    // const { _id } = req.params; // 
+
+    User.findOne({/** parames here */})
+      .then(student => {
+        const { firstName, lastName, age } = student;
+        res.locals.student = { firstName, lastName, age };
+        return next();
+      })
+      .catch((err) => {
+        return next(createErr({
+          method: 'getCharacters',
+          type: 'retrieving mongoDB data',
+          err, 
+        }));
+      });
+}
+
+
+// ADD MIDDLEWARE TO CREATE USER
+userController.createUser = (req, res, next) => {
+    console.log('---We are in createUser in userController.js--');
+
+    const { username, password, trips } = req.body; 
+    const newUser = new User({ username, password, trips });
+
+    newStudent.save()
+        .then(newUser => {
+            const { username, password, trips } = newUser;
+            res.locals.user = { firstName, lastName, age };
+            return next();
+        })
+        .catch((err) => {
+            return next(createErr({
+            method: 'addUser',
+            type: 'adding newUser to mongoDB data',
+            err, 
+            }));
+        });
 };
 
 
 // ADD MIDDLEWARE TO DELETE USER
-characterController.deleteTrip = (req, res, next) => {
+userController.deleteUser = (req, res, next) => {
     console.log('---We are in deleteUser in userController.js----');
-    const { _id } = req.params; // always, always, always destructure for security reasons!!!
+
+    const { _id } = req.params; 
     console.log(_id);
-    res.locals.deleteId = _id;
-    return next();
+
+    User.findOneAndDelete({_id: _id})
+      .then(student => {
+        console.log(student);
+        const { firstName, lastName, age } = student;
+        res.locals.student = { firstName, lastName, age };
+        return next();
+      })
+      .catch((err) => {
+        return next(createErr({
+          method: 'deleteStudent',
+          type: 'retrieving mongoDB data',
+          err, 
+        }));
+      });
 };
--- server/server.js
+++ server/server.js
@@ -1,20 +1,30 @@
 const path = require('path');
 const express = require('express');
 
-
+// Routers
 const userRouter = require('./routes/user.js');
 const tripRouter = require('./routes/trip.js');
+
+
+const PORT = 3000;
+const app = express();
+
+
+// Parse all requests
 app.use(express.json()); 
 app.use(express.urlencoded({extended: true})); // important for forms!!
 
 
-
 // define route handlers 
-app.use('/', userRouter);
+// from Signup component -> auth routes to '/user/_id'
+app.use('/', userRouter); // from Signup component -> auth routes to '/user/_id'
 
-app.use('/trips', tripRouter);
+app.use('/signup', userRouter); // from Signup component -> auth routes to '/user/_id'
+
+app.use('/user/:_id', userRouter) // Access to trips from here
 
 
+app.use('/trips/:_id', tripRouter); // The main infographic page
 
 
 // catch-all route handler for any requests to an unknown route
@@ -29,6 +39,7 @@ app.use((err, req, res, next) => {
     const errorStatus = err.status || 500;
     return res.status(errorStatus).send(res.locals.message);
   });
+
 
 
   //  start server
