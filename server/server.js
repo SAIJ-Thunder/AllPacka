8dc214896fadacf1760d6056b112a09d15c16350
William Murphy <williamjmurphy@gmial.com>
2023-04-10 08:55:30 -0500

    fixing bugs

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
-// // Routers
+// Routers
 const tripRouter = require('./routes/tripRouter');
-// const userRouter = require('./routes/userRouter');
-
+const userRouter = require('./routes/userRouter');
 
 
 const PORT = 1234;
@@ -16,8 +17,9 @@ const app = express();
 app.use(express.json()); 
 app.use(express.urlencoded({extended: true})); // important for forms!!
 
-
-const MONGO_URI = 'mongodb+srv://olsoninoslo:Iamnedtito@cluster0.bslfwul.mongodb.net/?retryWrites=true&w=majority';
+const MONGO_URI = process.env.MARK_ALLPACKADB_MONGO_URL;
+// const MONGO_URI = 'error maker'
+// const MONGO_URI = 'mongodb+srv://olsoninoslo:Iamnedtito@cluster0.bslfwul.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose.connect(MONGO_URI, {
   // options for the connect method to parse the URI
@@ -32,31 +34,32 @@ mongoose.connect(MONGO_URI, {
 
 
 
-// // define route handlers 
-// // from Signup component -> auth routes to '/user/_id'
+// define route handlers 
+// from Signup component -> auth routes to '/user/_id'
 // app.use('/', userRouter); // from Signup component -> auth routes to '/user/_id'
 
 // app.use('/signup', userRouter); // from Signup component -> auth routes to '/user/_id'
 
-// app.use('/users', userRouter) // Access to trips from here
+app.use('/user', userRouter) // Access to trips from here
 
 
-// app.use('/trips', tripRouter); // The main infographic page
+app.use('/trip', tripRouter); // The main infographic page
 
 
-// // catch-all route handler for any requests to an unknown route
-// app.use((req,res) => res.sendStatus(404));
+// catch-all route handler for any requests to an unknown route
+app.use((req,res) => res.status(404).send("Big ol' fail"));
 
 
-// app.use((err, req, res, next) => {
-//     // this is the default error obj
-//     console.log('We have entered the twightlight Zone!');
-//     res.locals.message = err.message;
-//     console.log('Our error log is: ', err.log)
-//     // console.log('Our error message is: ', err.message);
-//     const errorStatus = err.status || 500;
-//     return res.status(errorStatus).send(res.locals.message);
-//   });
+app.use((err, req, res, next) => {
+    // this is the default error obj
+    console.log('We have entered the twightlight Zone!');
+    res.locals.message = err.message;
+    console.log('Our error log is: ', err.log)
+    // console.log('Our error message is: ', err.message);
+    const errorStatus = err.status || 500;
+    return res.status(errorStatus).send(res.locals.message);
+  });
+
 
 
   //  start server
