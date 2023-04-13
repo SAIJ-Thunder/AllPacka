9b324d9f17caaa245d317e779a0ada66dfe620a7
wiris316 <wiris316@gmail.com>
2023-04-12 21:12:32 -0400

    Added categories, started additem middleware
    Co-authored-by: Sophia Bui <sophia-bui@users.noreply.github.com>
    Co-authored-by: Jasmine Noor <jasminee@gmail.com>
    Co-authored-by: Anne-lise Emig <anneliseemig@gmail.com>

--- client/pages/TripHome/categoryComponent.jsx
+++ client/pages/TripHome/categoryComponent.jsx
@@ -31,16 +31,24 @@ const CategoryComponent = () => {
   //---------------------------------------------------- NEW----------------------------------------------------
 
 
-
+  const [category, setCategory] = useState('');
   const [items, setItems] = useState([])
   const addItems=() => {
-    const newItems = [...items, <AddItemsComponent key={items.length} />];
+    const newItems = [...items, <AddItemsComponent category={category} key={items.length} />];
     setItems(newItems);
   }
 
 
   return(
     <div className="add-new-category">
+      <div>
+      <label htmlFor="category">Category:</label>
+      <select name="category" onChange={(e)=>setCategory(e.target.value)}>
+        <option value="food">Food</option>
+        <option value="drinks">Drinks</option>
+        <option value="snacks">Snacks</option>
+      </select>
+      </div>
         <button className="category-button" id="add-item-button" onClick={addItems}>Add Item</button>
         <div> 
           {items}
